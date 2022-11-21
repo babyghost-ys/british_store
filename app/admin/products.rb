2f8ea39adc6460b2360a00ccbc24e9906b14491f
Peter <babyghost@gmail.com>
2022-11-21 16:43:43 -0600

    Added the product model in the ActiveAdmin.

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :sku, :name, :description, :current_price, :status
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:sku, :name, :description, :current_price, :status]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
