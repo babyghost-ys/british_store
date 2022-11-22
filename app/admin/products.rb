23d94a864e626582cfcdce9f28fb245d82a714c5
Peter <babyghost@gmail.com>
2022-11-22 17:24:41 -0600

    Fixed permission rules of Product of ActiveAdmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :sku, :name, :description, :current_price, :status
+  permit_params :sku, :name, :description, :current_price, :status, :category_id
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
