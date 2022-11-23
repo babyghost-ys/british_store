add4b68474d7aaed98269f20599a14e9b72b0d25
Peter <babyghost@gmail.com>
2022-11-22 22:57:30 -0600

    Added some basic code to show the Products.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'products/index'
-  get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "products#index"
+
+  resources :products, only: [:index, :show]
 end
