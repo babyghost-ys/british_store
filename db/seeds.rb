a2e3c84ad06b7e7cd85fff84120e4f3f9f16e01d
Peter <babyghost@gmail.com>
2022-11-23 02:08:56 -0600

    Updated seed file to create categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.delete_all
+Category.delete_all
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Category.find_or_create_by(name: "Clothes")
+Category.find_or_create_by(name: "Home & Decor")
+Category.find_or_create_by(name: "Tea")
+Category.find_or_create_by(name: "British Snacks")
+Category.find_or_create_by(name: "Art and Collectibles")
+Category.find_or_create_by(name: "Greeting Cards")
+Category.find_or_create_by(name: "Others")
