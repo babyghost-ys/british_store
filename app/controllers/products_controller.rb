21a5d78d14ba6ae94eaeb985d54c51033c57fa2a
Peter <babyghost@gmail.com>
2022-11-26 06:13:44 -0600

    Added the dropdown list for search.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page])
+    @categories = Category.all
   end
 
   def show
@@ -12,6 +13,12 @@ class ProductsController < ApplicationController
     @param_catid = params[:id]
     @categories = Category.all
     wildcard_search = "%#{params[:q]}%"
-    @products = Product.where("name LIKE ? OR description LIKE ?", wildcard_search, wildcard_search).page(params[:page])
+    category_id = "#{params[:id]}"
+
+    if (category_id == "")
+      @products = Product.where("name LIKE ? OR description LIKE ?", wildcard_search, wildcard_search).page(params[:page])
+    else
+      @products = Product.where("name LIKE ? OR description LIKE ?", wildcard_search, wildcard_search).where(categories: category_id).page(params[:page])
+    end
   end
 end
