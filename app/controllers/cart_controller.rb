d6ad59e91ae0f47971dec590838ee0dce7e28523
Peter <babyghost@gmail.com>
2022-11-28 04:36:37 -0600

    Updated several shopping cart design.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,13 @@
 class CartController < ApplicationController
   def create
     current_id = params[:id]
+    @product = Product.find(current_id)
     if session[:shopping_cart].map { |item| item["id"] }.include?(current_id)
-      session[:shopping_cart].find { |item| item["id"] == current_id }["quantity"] = 300
-      flash[:notice] = "You already added this item at index to your cart. #{session[:shopping_cart]} "
+      # session[:shopping_cart].find { |item| item["id"] == current_id }["quantity"] = 1
+      flash[:notice] = "You already added this item at index to your cart. Click View Cart to change the quantity. "
     else
       session[:shopping_cart] << { "id" => params[:id], "quantity" => params[:quantity].to_i }
-      flash[:notice] = "#{session[:shopping_cart]} added to cart"
+      flash[:notice] = "#{@product.name} have been added to the cart. Click View Cart to change the quantity."
     end
 
     redirect_to product_path(current_id)
@@ -15,14 +16,24 @@ class CartController < ApplicationController
   def destroy
     current_id = params[:id]
     session[:shopping_cart].delete_if { |item| item["id"] == current_id }
-    redirect_to cart_index_path()
+    if product_path(current_id)
+      redirect_to product_path(current_id)
+      flash[:notice] = "Item removed from cart."
+    else
+      redirect_to cart_index_path()
+    end
   end
 
   def update
     current_id = params[:id]
     update_quantity = params[:update_quantity].to_i
     session[:shopping_cart].find { |item| item["id"] == current_id }["quantity"] = update_quantity
-    redirect_to cart_index_path()
+    if product_path(current_id)
+      redirect_to product_path(current_id)
+      flash[:notice] = "Item quantity updated."
+    else
+      redirect_to cart_index_path()
+    end
   end
 
   def index
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    current_id = params[:id]
+    @product = Product.find(current_id)
     @categories = Category.all
-
     @cart = session[:shopping_cart]
+
+    if @cart.map { |item| item["id"] }.include?(current_id)
+      @already_in_cart = true
+      @current_item_quantity = @cart.find { |item| item["id"] == current_id }["quantity"]
+    end
   end
 
   def search
