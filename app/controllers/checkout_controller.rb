f0a0512985892b433f16f7a43f0caca8ab86371b
Peter <babyghost@gmail.com>
2022-11-28 13:13:08 -0600

    Added Stripe testing code.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,56 @@
 class CheckoutController < ApplicationController
+
+  def create
+    # Establish a connection with Stripe and then redirect the user to the payment
+
+    # Create Stripe Session
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
+      line_items: [
+        {
+          price_data: {
+            currency: 'cad',
+            unit_amount: 123_4,
+            product_data: {
+              name: "test",
+              description: "product.description",
+            },
+            tax_behavior: 'exclusive',
+          },
+          quantity: 1,
+        },
+        {
+          price_data: {
+            currency: 'cad',
+            unit_amount: 50_01,
+            product_data: {
+              name: "Another Product",
+              description: "test123",
+            },
+            tax_behavior: 'exclusive',
+          },
+          quantity: 1,
+        },
+
+      ],
+      automatic_tax: {
+        enabled: true
+      },
+      mode: 'payment',
+      billing_address_collection: 'required',
+    )
+
+    # Redirect to the original page
+    redirect_to @session.url, allow_other_host: true
+  end
+
+  def success
+
+  end
+
+  def cancel
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,10 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
 
   resources :cart, only: [:index, :create, :destroy, :update]
+
+  scope "/checkout" do
+    post "/create", to: "checkout#create", as: "checkout_create"
+    get "/success", to: "checkout#success", as: "checkout_success"
+    get "/cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
 end
