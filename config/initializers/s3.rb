57ea8779e81772b0f83c39746d3cc752430c951a
Peter <babyghost@gmail.com>
2022-11-30 21:44:53 -0600

    Modified config for the newest S3 gem.

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,6 @@
-Aws::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
+require 'aws-sdk'
+
+Aws.config.update(
+  access_key_id: ENV['S3_KEY'],
+  secret_access_key: ENV['S3_SECRET'],
 )
