c1fd8133d345df3dd16435610a088990a09f1c41
Peter <babyghost@gmail.com>
2022-12-09 01:07:15 -0600

    Added order status in the ActiveAdmin.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   validates :amount_total, presence: true, numericality: { greater_than: 0 }
   validates :amount_subtotal, presence: true, numericality: { greater_than: 0 }
 
-  belongs_to :customer, :order_status
+  belongs_to :customer
+  belongs_to :order_status
   has_many :order_details, dependent: :delete_all
 end
