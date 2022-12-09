93d8477b568da72601e7c7c8333a0aabe9651c33
Peter <babyghost@gmail.com>
2022-12-08 22:53:55 -0600

    Added responsive design for the flash notice.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,7 +7,8 @@ class CartController < ApplicationController
       flash[:notice] = "You already added this item at index to your cart. Click View Cart to change the quantity. "
     else
       session[:shopping_cart] << { "id" => params[:id], "quantity" => params[:quantity].to_i }
-      flash[:notice] = "#{@product.name} have been added to the cart. Click View Cart to change the quantity."
+      flash[:notice] = "#{@product.name} is added to the cart."
+      flash[:mobile_notice] = "Added to cart."
     end
 
     redirect_to product_path(current_id)
@@ -19,7 +20,8 @@ class CartController < ApplicationController
     session[:shopping_cart].delete_if { |item| item["id"] == current_id }
     if curent_page == "products"
       redirect_to product_path(current_id)
-      flash[:notice] = "Item removed from cart."
+      flash[:notice] = "This item is removed from cart."
+      flash[:mobile_notice] = "Removed."
     else
       redirect_to cart_index_path()
     end
@@ -32,7 +34,8 @@ class CartController < ApplicationController
     session[:shopping_cart].find { |item| item["id"] == current_id }["quantity"] = update_quantity
     if curent_page == "products"
       redirect_to product_path(current_id)
-      flash[:notice] = "Item quantity updated."
+      flash[:notice] = "This item's quantity has been updated."
+      flash[:mobile_notice] = "Quantity updated."
     else
       redirect_to cart_index_path()
     end
