7254d8e0eda9fe955c2bcc7bff533ad572590f4c
Peter <babyghost@gmail.com>
2022-12-09 10:55:04 -0600

    Fixed the validation error.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, :description, :current_price, presence: true
-  validates :current_price, numericality: { greater_than: 0 }
+  validates :current_price, numericality: true
   belongs_to :category
   has_one_attached :image
   has_many :order_detail, dependent: :delete_all
