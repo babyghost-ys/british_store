ca53dd07e14d7242e1454bee08586d57a20bee34
Peter <babyghost@gmail.com>
2022-12-09 00:57:45 -0600

    Moved the Order Status column in a new table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_092916) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_065506) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -96,6 +96,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_092916) do
     t.index ["product_id"], name: "index_order_details_on_product_id"
   end
 
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "payment_intent"
     t.string "payment_status"
@@ -104,7 +110,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_092916) do
     t.float "amount_subtotal"
     t.integer "unix_timestamp"
     t.string "stripe_status"
-    t.string "order_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id", null: false
