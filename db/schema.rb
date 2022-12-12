400b3f4710c700864905c68bffcb0ed241704e5b
Peter <babyghost@gmail.com>
2022-12-11 21:59:06 -0600

    Removed the unncessary schema.

--- db/schema.rb
+++ db/schema.rb
@@ -85,18 +85,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_032705) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "members", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_members_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
-  end
-
   create_table "order_details", force: :cascade do |t|
     t.integer "quantity"
     t.float "purchase_unit_price"
